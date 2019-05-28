d977ea85b87b07b0969cef1c98ee2cf6a4d11d6f
Ayoub Karioun <kariounayoub@gmail.com>
2019-05-28 22:11:13 +0000

    seed

--- db/schema.rb
+++ db/schema.rb
@@ -27,26 +27,6 @@ ActiveRecord::Schema.define(version: 2019_05_27_115318) do
   create_table "inscriptions", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "evenement_id"
-    t.text "q1"
-    t.text "r1"
-    t.text "q2"
-    t.text "r2"
-    t.text "q3"
-    t.text "r3"
-    t.text "q4"
-    t.text "r4"
-    t.text "q5"
-    t.text "r5"
-    t.text "q6"
-    t.text "r6"
-    t.text "q7"
-    t.text "r7"
-    t.text "q8"
-    t.text "r8"
-    t.text "q9"
-    t.text "r9"
-    t.text "q10"
-    t.text "r10"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["evenement_id"], name: "index_inscriptions_on_evenement_id"
@@ -55,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_05_27_115318) do
 
   create_table "questions", force: :cascade do |t|
     t.bigint "evenement_id"
+    t.string "categorie"
     t.text "contenu"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
