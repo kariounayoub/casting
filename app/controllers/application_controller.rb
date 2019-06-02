8fbe7708a8e71ce169727d84a9d8a52144954f30
Ayoub Karioun <kariounayoub@gmail.com>
2019-06-02 15:34:53 +0000

    add data to user and sanitizers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:prenom, :nom, :adresse, :ville, :date_de_naissance, :sexe, :taille, :taille_vetement, :pointure, :nationalite_residence, :allergie_intolerance, :allergie_intolerance_contenu, :medicament, :medicament_contenu, :autre])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:prenom, :nom, :adresse, :ville, :date_de_naissance, :sexe, :taille, :taille_vetement, :pointure, :nationalite_residence, :allergie_intolerance, :allergie_intolerance_contenu, :medicament, :medicament_contenu, :autre])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_230810) do
+ActiveRecord::Schema.define(version: 2019_06_02_143515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,21 @@ ActiveRecord::Schema.define(version: 2019_05_28_230810) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "prenom"
+    t.string "nom"
+    t.string "adresse"
+    t.string "ville"
+    t.date "date_de_naissance"
+    t.string "sexe"
+    t.integer "taille"
+    t.string "taille_vetement"
+    t.integer "pointure"
+    t.boolean "nationalite_residence"
+    t.boolean "allergie_intolerance"
+    t.text "allergie_intolerance_contenu"
+    t.boolean "medicament"
+    t.text "medicament_contenu"
+    t.text "autre"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
