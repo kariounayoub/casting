d1eab07d40bfa7a10b75b5778309d9ee903fbff5
Ayoub Karioun <kariounayoub@gmail.com>
2019-06-02 14:34:38 +0000

    evenement question inscription reponse CRUD done

--- app/models/inscription.rb
+++ app/models/inscription.rb
@@ -1,6 +1,7 @@
 class Inscription < ApplicationRecord
   belongs_to :user
   belongs_to :evenement
-  has_many :reponses
+  has_many :reponses, dependent: :destroy
+  # has_many :questions, through: :reponses
   accepts_nested_attributes_for :reponses
 end
