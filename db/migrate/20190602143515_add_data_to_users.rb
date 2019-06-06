6d1d97e2e1fcc6c39deda9622464445a0eea492c
Ayoub Karioun <kariounayoub@gmail.com>
2019-06-06 16:55:34 +0000

    i18n home and navbar

--- db/migrate/20190602143515_add_data_to_users.rb
+++ db/migrate/20190602143515_add_data_to_users.rb
@@ -6,14 +6,5 @@ class AddDataToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :ville, :string
     add_column :users, :date_de_naissance, :date
     add_column :users, :sexe, :string
-    add_column :users, :taille, :integer
-    add_column :users, :taille_vetement, :string
-    add_column :users, :pointure, :integer
-    add_column :users, :nationalite_residence, :boolean
-    add_column :users, :allergie_intolerance, :boolean
-    add_column :users, :allergie_intolerance_contenu, :text
-    add_column :users, :medicament, :boolean
-    add_column :users, :medicament_contenu, :text
-    add_column :users, :autre, :text
   end
 end
