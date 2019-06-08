784bf2b8d34db2504286bf7cbafa6e60bbc7314f
Ayoub Karioun <kariounayoub@gmail.com>
2019-06-08 15:09:28 +0000

    cloudinary not working yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_055413) do
+ActiveRecord::Schema.define(version: 2019_06_08_143211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_055413) do
     t.date "naissance"
     t.string "sexe"
     t.boolean "admin"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
