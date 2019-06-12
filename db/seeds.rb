01f2a52324bdb0f5601d6f4dd5a4f25cd27a82bd
Ayoub Karioun <kariounayoub@gmail.com>
2019-06-12 21:55:28 +0100

    home page content arabe

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,31 +17,25 @@ puts 'creating questions'
 
 mcjr2 = Evenement.create(nom: 'MCJR2', date: '15/06/2019', adresse: 'Hotel Grand Mogador, Casablanca', actif: true)
 
-Question.create(evenement: mcjr2, categorie: 'personnel', contenu: 'taille', data_type: 'string', required: true)
-Question.create(evenement: mcjr2, categorie: 'personnel', contenu: 'vetement', data_type: 'string', required: true)
-Question.create(evenement: mcjr2, categorie: 'personnel', contenu: 'pointure', data_type: 'string')
-Question.create(evenement: mcjr2, categorie: 'personnel', contenu: 'nationalite_residence', data_type: 'string')
 Question.create(evenement: mcjr2, categorie: 'personnel', contenu: 'allergie_intolerance', data_type: 'boolean')
 Question.create(evenement: mcjr2, categorie: 'personnel', contenu: 'allergie_intolerance_contenu', data_type: 'text')
 Question.create(evenement: mcjr2, categorie: 'personnel', contenu: 'medicament', data_type: 'boolean')
 Question.create(evenement: mcjr2, categorie: 'personnel', contenu: 'medicament_contenu', data_type: 'text')
 Question.create(evenement: mcjr2, categorie: 'personnel', contenu: 'autre', data_type: 'text')
 
-Question.create(evenement: mcjr2, categorie: 'tuteur', contenu: 'nom', data_type: 'string')
-Question.create(evenement: mcjr2, categorie: 'tuteur', contenu: 'prenom', data_type: 'string')
+Question.create(evenement: mcjr2, categorie: 'tuteur', contenu: 'nom', data_type: 'string', required: true)
+Question.create(evenement: mcjr2, categorie: 'tuteur', contenu: 'prenom', data_type: 'string', required: true)
 Question.create(evenement: mcjr2, categorie: 'tuteur', contenu: 'lien', data_type: 'string')
 Question.create(evenement: mcjr2, categorie: 'tuteur', contenu: 'adresse', data_type: 'text')
 Question.create(evenement: mcjr2, categorie: 'tuteur', contenu: 'ville', data_type: 'string')
-Question.create(evenement: mcjr2, categorie: 'tuteur', contenu: 'tel', data_type: 'tel')
-Question.create(evenement: mcjr2, categorie: 'tuteur', contenu: 'email', data_type: 'email')
+Question.create(evenement: mcjr2, categorie: 'tuteur', contenu: 'tel', data_type: 'tel', required: true)
+Question.create(evenement: mcjr2, categorie: 'tuteur', contenu: 'email', data_type: 'email', required: true)
 
-Question.create(evenement: mcjr2, categorie: 'ecole', contenu: 'nom', data_type: 'string')
+Question.create(evenement: mcjr2, categorie: 'ecole', contenu: 'nom', data_type: 'string', required: true)
 Question.create(evenement: mcjr2, categorie: 'ecole', contenu: 'adresse', data_type: 'text')
 Question.create(evenement: mcjr2, categorie: 'ecole', contenu: 'ville')
-Question.create(evenement: mcjr2, categorie: 'ecole', contenu: 'contact', data_type: 'string')
-Question.create(evenement: mcjr2, categorie: 'ecole', contenu: 'tel', data_type: 'tel')
 
-Question.create(evenement: mcjr2, categorie: 'motivation', contenu: 'participation', data_type: 'text')
+Question.create(evenement: mcjr2, categorie: 'motivation', contenu: 'participation', data_type: 'text', required: true)
 Question.create(evenement: mcjr2, categorie: 'motivation', contenu: 'competitif', data_type: 'text')
 
 Question.create(evenement: mcjr2, categorie: 'cuisine', contenu: 'temps', data_type: 'text')
@@ -61,7 +55,9 @@ Question.create(evenement: mcjr2, categorie: 'competence', contenu: 'habitude', 
 
 Question.create(evenement: mcjr2, categorie: 'perso', contenu: 'fraterie', data_type: 'text')
 Question.create(evenement: mcjr2, categorie: 'perso', contenu: 'classe', data_type: 'text')
+Question.create(evenement: mcjr2, categorie: 'perso', contenu: 'sports', data_type: 'text')
 Question.create(evenement: mcjr2, categorie: 'perso', contenu: 'loisirs', data_type: 'text')
+Question.create(evenement: mcjr2, categorie: 'perso', contenu: 'assos', data_type: 'text')
 Question.create(evenement: mcjr2, categorie: 'perso', contenu: 'grand', data_type: 'text')
 Question.create(evenement: mcjr2, categorie: 'perso', contenu: 'qualites', data_type: 'text')
 Question.create(evenement: mcjr2, categorie: 'perso', contenu: 'defauts', data_type: 'text')
