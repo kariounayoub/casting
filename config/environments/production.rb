57eef13b7d585346290b7cb0c8c44c2b09428b46
Majid Jaidi <majid.jaidi@gmail.com>
2019-06-25 19:15:19 +0100

    action mailer heroku asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://www.masterchefmaroc.ma'
-  config.action_mailer.asset_host = 'http://www.masterchefmaroc.ma'
+  config.action_mailer.asset_host = 'https://casting-mcm.herokuapp.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
