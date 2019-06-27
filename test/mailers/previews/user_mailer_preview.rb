cea5ddd2667fd52d217cb9fb849408da951759b8
Ayoub Karioun <kariounayoub@gmail.com>
2019-06-27 10:16:09 +0200

    mail content ok for welcome inscription and convocation

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -5,7 +5,7 @@ class UserMailerPreview < ActionMailer::Preview
   end
 
   def inscription
-    user = User.first
+    user = User.last
     inscription = user.inscription.last
     UserMailer.with(inscription: inscription).inscription
   end
