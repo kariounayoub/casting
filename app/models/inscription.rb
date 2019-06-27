bc91bc0dd4d44c199e2c28aae68abdcbbaab3612
Majid Jaidi <majid.jaidi@gmail.com>
2019-06-27 10:14:06 +0100

    fix nil note dashboard

--- app/models/inscription.rb
+++ app/models/inscription.rb
@@ -12,11 +12,11 @@ class Inscription < ApplicationRecord
   mount_base64_uploader :photo_3, PhotoUploader
 
   def avg_note_cuisine
-    (self.notes.map{|n| n.note_cuisine}&.sum.to_f / self.notes.map{|n| n.note_cuisine}&.size)
+    (self.notes.map{|n| n.note_cuisine.nil? ? 0 : n.note_cuisine}&.sum.to_f / self.notes.map{|n| n.note_cuisine}&.size)
   end
 
   def avg_note_personalite
-    (self.notes.map{|n| n.note_personalite}&.sum.to_f / self.notes.map{|n| n.note_personalite}&.size)
+    (self.notes.map{|n| n.note_personalite.nil? ? 0 : n.note_personalite}&.sum.to_f / self.notes.map{|n| n.note_personalite}&.size)
   end
 
   def percent_complete
